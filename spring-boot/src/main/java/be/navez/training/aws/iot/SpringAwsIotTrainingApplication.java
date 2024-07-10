1b8cf6ce9ecf5192cd0b6c24777404f1dbc56246
Jerome Navez <ext-jena@hms-networks.com>
2024-07-10 17:21:45 +0200

    spring project structure

---
+++ spring-boot/src/main/java/be/navez/training/aws/iot/SpringAwsIotTrainingApplication.java
@@ -0,0 +1,13 @@
+package be.navez.training.aws.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsIotTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsIotTrainingApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/be/navez/training/aws/iot/SpringAwsIotTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.navez.training.aws.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsIotTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
